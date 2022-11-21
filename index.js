ab8666a6909b4c2d76851e01c047106acd8df74d
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2022-11-20 21:52:29 -0500

    dom manipulation part 14

--- index.js
+++ index.js
@@ -10,68 +10,25 @@ DOMSelectors.button.addEventListener("click", function () {
   let input1 = DOMSelectors.input1.value;
   let input2 = DOMSelectors.input2.value;
   let input3 = DOMSelectors.input3.value;
-  if (input3 === "") {
-  } else if (input3 == "Monster") {
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      ` <div id="center">
-    <h2>${input1} </h2>
-    <img src="monster.png" alt="monster">
-    <p> By:${input2}  </p><button id="red" >Delete</button </div>
-    `
-    );
-    DOMSelectors.input1.value = ``;
-    DOMSelectors.input2.value = ``;
-    DOMSelectors.input3.value = ``;
-  } else if (input3 == "Shark") {
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      ` <div id="center">
-    <h2>${input1} </h2>
-    <img src="shark.png" alt="shark">
-    <p> By:${input2}  </p><button id="red">Delete</button </div>
-    `
-    );
-    DOMSelectors.input1.value = ``;
-    DOMSelectors.input2.value = ``;
-    DOMSelectors.input3.value = ``;
-  } else if (input3 == "Dude With A Gun") {
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      ` <div id="center">
-    <h2>${input1} </h2>
-    <img src="dudewithagun.png" alt="dudewithagun">
-    <p> By:${input2}  </p><button id="red" >Delete</button </div>
-    `
-    );
-    DOMSelectors.input1.value = ``;
-    DOMSelectors.input2.value = ``;
-    DOMSelectors.input3.value = ``;
-  } else if (input3 == "Person Sitting On A Bench") {
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      ` <div id="center">
-    <h2>${input1} </h2>
-    <img src="PersonSittingonabench.png" alt="PersonSittingonabench">
-    <p> By:${input2}  </p><button id="red" >Delete</button </div>
-    `
-    );
-    DOMSelectors.input1.value = ``;
-    DOMSelectors.input2.value = ``;
-    DOMSelectors.input3.value = ``;
-  } else {
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      ` <div id="center">
-    <h2>${input1} </h2>
-    <img src="Couple.png" alt="couple">
-    <p> By:${input2}  </p><button id="red" >Delete</button </div>
-    `
-    );
-    DOMSelectors.input1.value = ``;
-    DOMSelectors.input2.value = ``;
-    DOMSelectors.input3.value = ``;
-  }
-});
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<div class="output">
+    <h3>${input1}</h3>
+    <img src="${input3}.png" alt="a">
+     <h3>${input2}</h3>
+    <button class="remove">Remove</button>
+    </div>`
+  );
 
 
+  const removes = document.querySelectorAll(".remove");
+  const outputs = document.querySelector(".output");
+  removes.forEach((eachRemove) => {
+    eachRemove.addEventListener("click", () => {
+
+      outputs.remove();
+      
+   
+    });
+  });
+}); 
